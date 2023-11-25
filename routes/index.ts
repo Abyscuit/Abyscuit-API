d2f6ce1f118ea3410797d9a7cd3fda754050247b
Abyscuit <marcus14530@gmail.com>
2023-11-24 23:51:41 -0800

    devop: add authorize discord API

--- routes/index.ts
+++ routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import users from './users';
 import gpt from './gpt';
+import authorize from './authorize';
 
 const router = express.Router();
 
 router.use('/users', users);
 router.use('/gpt', gpt);
+router.use('/authorize', authorize);
 
 export default router;
