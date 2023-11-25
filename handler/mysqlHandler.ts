a9a679bfb8f3e37024ca06d467ff03efefc38e32
Abyscuit <marcus14530@gmail.com>
2023-11-24 22:11:36 -0800

    devop: create query handler

--- handler/mysqlHandler.ts
+++ handler/mysqlHandler.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Create the connection to the database
 const connection = mysql.createConnection(process.env.DATABASE_URL ?? '');
@@ -11,6 +14,45 @@ export function showTables() {
   });
 }
 
-export function createDatabase() {
-  connection.query('');
+// Use later
+// export function importAccounts() {
+//   console.table(accounts);
+//   const query = `INSERT INTO users (discordID, username, credits, email, isUnlimited) VALUES ?`;
+//   const values = accounts.map(item => {
+//     const value = [item.discordID, item.username, item.credits, 'null', false];
+
+//     return value;
+//   });
+//   connection.query(query, [values], (err, results, fields) => {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra metadata about results, if available
+//     console.log(err);
+//   });
+// }
+
+export function runQuery(query: string, callback: Function) {
+  connection.query(query, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    // console.log('results:\n', results);
+    return callback(results); // results contains rows returned by server
+  });
+}
+export function runQueryValues(
+  query: string,
+  values: unknown,
+  callback: Function
+) {
+  const results = connection.query(query, [values], (err, results) => {
+    if (err) {
+      throw err;
+    }
+    return callback(results); // results contains rows returned by server
+  });
+  return results;
+}
+
+export function closeConnection() {
+  connection.end();
 }
