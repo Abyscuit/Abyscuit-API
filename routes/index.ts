6fb5e536c9011cb24818f251a004c289d3ab6a68
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2024-08-02 15:14:43 -0700

    chore: comment out users until database migration

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import users from './users';
+// import users from './users';
 import gpt from './gpt';
 import authorize from './authorize';
 
 const router = express.Router();
 
-router.use('/users', users);
+// router.use('/users', users);
 router.use('/gpt', gpt);
 router.use('/authorize', authorize);
 
